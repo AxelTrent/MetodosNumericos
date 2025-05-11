b65c799d398498c91e901f7169a61db9cb687579
AxelTrent <23030087@itesa.edu.mx>
2025-05-11 17:16:14 -0600

    Update GaussJordan.py
    Se implementa el gaussjordan

--- T3_Sistema_De_Ecuaciones/Métodos_Directos/GaussJordan.py
+++ T3_Sistema_De_Ecuaciones/Métodos_Directos/GaussJordan.py
@@ -25,3 +25,21 @@ def main():
 
     print("\nMatriz aumentada ingresada:")
     mostrar_matriz(matriz, soluciones, n)
+
+ # Método de Gauss-Jordan
+    for i in range(n):
+        pivote = matriz[i][i]
+        print(f"Normalizando fila {i + 1} dividiendo por {pivote:.3f}")
+        for j in range(n):
+            matriz[i][j] /= pivote
+        soluciones[i] /= pivote
+        mostrar_matriz(matriz, soluciones, n)
+
+        for k in range(n):
+            if k != i:
+                factor = matriz[k][i]
+                print(f"Eliminando elemento [{k + 1}][{i + 1}] con factor {factor:.3f}")
+                for j in range(n):
+                    matriz[k][j] -= factor * matriz[i][j]
+                soluciones[k] -= factor * soluciones[i]
+                mostrar_matriz(matriz, soluciones, n)
