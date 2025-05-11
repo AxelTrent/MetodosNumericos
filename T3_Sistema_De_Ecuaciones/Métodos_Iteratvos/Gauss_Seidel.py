53db49f0c34578c46cfc62e64f002e903abacfc5
AxelTrent <23030087@itesa.edu.mx>
2025-05-11 17:39:00 -0600

    Update Gauss_Seidel.py
    Se crea la matriz

--- T3_Sistema_De_Ecuaciones/Métodos_Iteratvos/Gauss_Seidel.py
+++ T3_Sistema_De_Ecuaciones/Métodos_Iteratvos/Gauss_Seidel.py
@@ -1,1 +1,7 @@
+def main():
+    n = int(input("Ingrese el número de ecuaciones (n): "))
 
+    matriz = [[0.0 for _ in range(n)] for _ in range(n)]
+    soluciones = [0.0 for _ in range(n)]
+    x = [0.0 for _ in range(n)]
+    anterior = [0.0 for _ in range(n)]
