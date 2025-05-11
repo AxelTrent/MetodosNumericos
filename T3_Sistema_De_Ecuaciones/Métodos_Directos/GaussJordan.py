0bca614aca0fef937020df5584aef845816a0635
AxelTrent <23030087@itesa.edu.mx>
2025-05-11 17:05:49 -0600

    Update GaussJordan.py
    Se agrego ingresar el numero de ecuaciones

--- T3_Sistema_De_Ecuaciones/Métodos_Directos/GaussJordan.py
+++ T3_Sistema_De_Ecuaciones/Métodos_Directos/GaussJordan.py
@@ -5,3 +5,12 @@ def mostrar_matriz(matriz, soluciones, n):
             print(f"{matriz[i][j]:8.3f} ", end="")
         print(f"| {soluciones[i]:8.3f} |")
     print()
+    
+def main():
+    
+    # Leer número de ecuaciones
+    n = int(input("Introduce el número de ecuaciones (n): "))
+
+    matriz = [[0.0 for _ in range(n)] for _ in range(n)]
+    soluciones = [0.0 for _ in range(n)]
+    x = [0.0 for _ in range(n)]
