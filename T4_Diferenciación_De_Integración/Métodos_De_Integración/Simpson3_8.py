fdc52a8d5686dda67e950375aec66745efad7573
AxelTrent <23030087@itesa.edu.mx>
2025-05-12 07:34:05 -0600

    Update Simpson3_8.py
    Verifica que el número de segmentos n sea múltiplo de 3, como lo requiere el método de Simpson 3/8

--- T4_Diferenciación_De_Integración/Métodos_De_Integración/Simpson3_8.py
+++ T4_Diferenciación_De_Integración/Métodos_De_Integración/Simpson3_8.py
@@ -4,3 +4,19 @@ import math
 def funcion(x):
     return x * math.sin(x)
 
+# Aplicación del método de Simpson 3/8
+def simpson38(a, b, n):
+    if n % 3 != 0:
+        raise ValueError("n debe ser múltiplo de 3")
+
+    h = (b - a) / n
+    suma = funcion(a) + funcion(b)
+
+    for i in range(1, n):
+        x = a + i * h
+        if i % 3 == 0:
+            suma += 2 * funcion(x)
+        else:
+            suma += 3 * funcion(x)
+
+    return (3 * h / 8) * suma
