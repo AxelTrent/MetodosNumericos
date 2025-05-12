08006d11340da8314967c2b6fe64068384143ffd
AxelTrent <23030087@itesa.edu.mx>
2025-05-12 00:08:31 -0600

    Update Cinco_Puntos.py
    Uso del esquema de cinco puntos con formato limpio.

--- T4_Diferenciación_De_Integración/Métodos_De_Difereciación/Cinco_Puntos.py
+++ T4_Diferenciación_De_Integración/Métodos_De_Difereciación/Cinco_Puntos.py
@@ -9,4 +9,12 @@ def cinco_puntos(x, h):
     fx2 = funcion(x + h)
     fx3 = funcion(x - h)
     fx4 = funcion(x - 2 * h)
-    
+     # Aplicar la fórmula de los Cinco Puntos
+    return (-fx1 + 8 * fx2 - 8 * fx3 + fx4) / (12 * h)
+
+def main():
+    # Definir parámetros fijos
+    x = 1
+    h = 0.1
+
+    resultado = cinco_puntos(x, h)
