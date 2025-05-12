938a3d406a594d591e3156a815496f71441e31a5
AxelTrent <23030087@itesa.edu.mx>
2025-05-12 00:06:41 -0600

    Update Cinco_Puntos.py
    Precisión con math.sin

--- T4_Diferenciación_De_Integración/Métodos_De_Difereciación/Cinco_Puntos.py
+++ T4_Diferenciación_De_Integración/Métodos_De_Difereciación/Cinco_Puntos.py
@@ -1,1 +1,12 @@
+import math
 
+def funcion(x):
+    return x * math.sin(x)
+
+def cinco_puntos(x, h):
+    # Evaluar la función en los puntos necesarios
+    fx1 = funcion(x + 2 * h)
+    fx2 = funcion(x + h)
+    fx3 = funcion(x - h)
+    fx4 = funcion(x - 2 * h)
+    
