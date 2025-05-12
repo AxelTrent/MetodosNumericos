81fb946d9f86dd5c481ea03c03ea9399f2c2ed56
AxelTrent <23030087@itesa.edu.mx>
2025-05-12 07:42:04 -0600

    Update Trapecio.py
    Calcula el tamaño del paso h
    Suma los extremos y valores intermedios
    Multiplica la suma total por h

--- T4_Diferenciación_De_Integración/Métodos_De_Integración/Trapecio.py
+++ T4_Diferenciación_De_Integración/Métodos_De_Integración/Trapecio.py
@@ -3,3 +3,18 @@ import math
 # Define la función a integrar
 def funcion(x):
     return x * math.sin(x)
+# Método del Trapecio compuesto
+def trapecio(a, b, n):
+    h = (b - a) / n
+    suma = (funcion(a) + funcion(b)) / 2
+
+    for i in range(1, n):  # Solo del 1 al n-1
+        x = a + i * h
+        suma += funcion(x)
+    
+    return h * suma
+
+# Parámetros de integración
+a = 0       # Límite inferior
+b = 2       # Límite superior
+n = 4       # Número de segmentos
