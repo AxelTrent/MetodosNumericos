4d958a1b28a2aec1286e84b76a546256af323d27
AxelTrent <23030087@itesa.edu.mx>
2025-05-20 22:19:05 -0600

    Update RegresionLineal.py
    Define los datos de entrada (x e y)
    
    Muestra los datos en formato tabular para visualización

--- T5_Interpolación/Método_Analisis_Datos/RegresionLineal.py
+++ T5_Interpolación/Método_Analisis_Datos/RegresionLineal.py
@@ -1,1 +1,14 @@
+def mostrar_datos(x, y):
+    print(" x\t y")
+    print("------------")
+    for i in range(len(x)):
+        print(f"{x[i]:.1f}\t{y[i]:.1f}")
+    print()
 
+def main():
+    # Datos de ejemplo
+    x = [1, 2, 3, 4, 5]
+    y = [2, 3, 5, 4, 6]
+    
+    # Mostrar datos en formato tabla
+    mostrar_datos(x, y)
